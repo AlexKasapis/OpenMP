04bf5690eb67954f53c98a12c50464ea5ad121d9
Alex Kasapis <Alex Kasapis>
2016-03-19 03:00:43 +0200

    Some variable fixed and parsing of fourth argument: how many threads shall omp use

--- Examine.c
+++ Examine.c
@@ -10,9 +10,28 @@
 #define MAX_VALUE 30
 #define SUCCESS 0
 #define FAILURE !SUCCESS
-#define NUMSIZE 4 //size of each float in the file
 
-long calculate_difference(struct timespec start, struct timespec end) {
+int check_arg_validity(char *argv[]) {
+	if(atoi(argv[1]) < -1) {
+		printf("First argument value is incorrect.\n");
+		return(FAILURE);
+	} else if(atoi(argv[2]) < -1) {
+		printf("Second argument value is incorrect.\n");
+		return(FAILURE);
+	} else if(atoi(argv[4]) < -1 || atoi(argv[4]) > 24) {
+		printf("Fourth argument value is incorrect.\n");
+		return(FAILURE);
+	} else if(atoi(argv[5]) < -1) {
+		printf("Fifth argument value is incorrect.\n");
+		return(FAILURE);
+	}
+	return(SUCCESS);
+}
+
+
+//Calculated the difference between two moments in seconds and nanosecs, and returns the
+//difference in total nanoseconds.
+long calculate_difference(struct timespec start, struct timespec end, int print_time) {
 	const int DAS_NANO_SECONDS_IN_SEC = 1000000000;
 	long timeElapsed_s = end.tv_sec - start.tv_sec;
 	long timeElapsed_n = end.tv_nsec - start.tv_nsec;
@@ -20,8 +39,10 @@ long calculate_difference(struct timespec start, struct timespec end) {
 		timeElapsed_n =	DAS_NANO_SECONDS_IN_SEC + timeElapsed_n;
 		timeElapsed_s--;
 	}
-	printf("Time: %ld.%09ld secs \n", timeElapsed_s, timeElapsed_n);
-	return timeElapsed_s;
+	if(print_time != 0)
+		printf("time: %ld.%09ld secs \n", timeElapsed_s, timeElapsed_n);
+	return timeElapsed_s*DAS_NANO_SECONDS_IN_SEC + timeElapsed_n
+;
 }
 
 //Checks if the coords are within the valid limits
@@ -47,52 +68,54 @@ off_t fsize(const char *filename) {
     return(FAILURE);
 }
 
-int parallel_motion_estimation(long num_coords, int seconds, char *file_name, size_t threads) {
+int parallel_motion_estimation(long num_coords, int max_seconds, char *file_name, int threads) {
 	// Check if file exists
 	if(access(file_name, F_OK) == -1) {
 		printf("File was not found!\n");
 		return(FAILURE);
 	}
+
+	if(threads >= 1)
+		omp_set_num_threads(threads);
 	#pragma omp parallel shared(file_name)
 	{
-		long exam_coords = fsize(file_name) / NUMSIZE / 3; //initialization of coordinations we are going to examine= total number
-		if(num_coords >=0 && num_coords<exam_coords){
-		exam_coords=num_coords;				
-	    }
-		if(num_coords>total_coords){
-		printf("You have asked for more lines than the ones available. All the lines are going to be examined.");
-	}
+		long exam_coords = fsize(file_name) / sizeof(float) / 3; //initialization of coordinations we are going to examine= total number
+		if(num_coords >=0 && num_coords < exam_coords)
+			exam_coords = num_coords;
+		if(num_coords > exam_coords)
+			printf("You have asked for more lines than the ones available. All the lines are going to be examined.\n");
+
 		int total_threads = omp_get_num_threads();
 		int tid = omp_get_thread_num();
 		FILE *file_ptr = fopen(file_name, "rb");
         struct timespec start, current;
 		
 		// Set which coords each thread will process
-		long coord_from = (int)exam_coords_coords/total_threads * tid;
-		long coord_to = (int)exam_coords_coords/total_threads * (tid+1) - 1;
+		long coord_from = (int)exam_coords/total_threads * tid;
+		long coord_to = (int)exam_coords/total_threads * (tid+1) - 1;
 		if(tid+1 == total_threads)
-			coord_to += total_coords % total_threads;
+			coord_to += exam_coords % total_threads;
 
 		fseek(file_ptr, 3*coord_from*sizeof(float), SEEK_SET);
 		long coords_read;
 		long valid_collisions=0;
-		if(tid==0){
-			
-			clock_gettime(CLOCK_MONOTONIC, &start);			
-		}
+
+		//if(tid==0)
+		clock_gettime(CLOCK_MONOTONIC, &start);
+
 		for(coords_read=coord_from; coords_read<coord_to+1; coords_read++) {
 			if(process_coords(file_ptr)==0)
 				valid_collisions++;
-			if(tid==0){
+			//if(tid==0 && max_seconds != -1) {
+			if(max_seconds != -1) {
 				clock_gettime(CLOCK_MONOTONIC, &current);
-				if(calculate_difference(start,current)>max_seconds){
-			    printf("Reached maximum time limit.");
-			    break;
+				if(calculate_difference(start, current, 0) > max_seconds) {
+					printf("Reached maximum time limit.\n");
+					break;
 				}
-		  		
-			}
-			     
+			}  
 		}
+
 		fclose(file_ptr);
 		printf("Thread %d, valid collisions %ld\n", tid, valid_collisions);
 	}
@@ -102,28 +125,26 @@ int parallel_motion_estimation(long num_coords, int seconds, char *file_name, si
 int linear_motion_estimation(long num_coords, int max_seconds, char *file_name) {
 	FILE *file_ptr = fopen(file_name, "rb");
 	if(!file_ptr) {
-		printf("Unable to open file!");
+		printf("Unable to open file!\n");
 		return(FAILURE);
 	}
+
     struct timespec start, current;
-	long exam_coords = fsize(file_name) / NUMSIZE / 3; //initialization of coordinations we are going to examine= total number
+	long exam_coords = fsize(file_name) / sizeof(float) / 3; //initialization of coordinations we are going to examine= total number
 	long curr_coord;
 	long valid_collisions=0;
-	if(num_coords >=0 && num_coords<total_coords){
-		exam_coords=num_coords;				
-	}
-	if(num_coords>total_coords){
-		printf("You have asked for more lines than the ones available. All the lines are going to be examined.");
-	}
-	if(max_seconds <= -1)
-		max_seconds=1000000;
+	if(num_coords>=0 && num_coords<exam_coords)
+		exam_coords=num_coords;		
+	if(num_coords > exam_coords)
+		printf("You have asked for more lines than the ones available. All the lines are going to be examined.\n");
+
 	clock_gettime(CLOCK_MONOTONIC, &start);
 	for(curr_coord=0; curr_coord<exam_coords; curr_coord++) {
 		if(process_coords(file_ptr)==0)
 			valid_collisions++;
 		clock_gettime(CLOCK_MONOTONIC, &current);
-		if(calculate_difference(start,current)>max_seconds){
-			printf("Reached maximum time limit.");
+		if(max_seconds != -1 && calculate_difference(start, current, 0)>max_seconds){
+			printf("Reached maximum time limit.\n");
 			break;
 		}
 	}
@@ -133,23 +154,35 @@ int linear_motion_estimation(long num_coords, int max_seconds, char *file_name) 
 }
 
 /*
- * argv[1] : number of collisions to examine
- * argv[2] : maximum running time
- * argv[3] : name of the data file
- * argv[4] : how many threads should openmp use (-1 = all available threads)
- * argv[5] : how many procs should openmpi use (-1 = all available procs)
+ * argv[1] : Maximum number of collisions to examine (-1 == all available collisions)
+ * argv[2] : Maximum running time in nanoseconds (-1 == until all collisions are checked)
+ * argv[3] : Name of the binary data file
+ * argv[4] : How many threads OMP is going to use (-1 == all available threads)
+ * argv[5] : How many procs OMPI is going to use (-1 = all available procs)
  */
 int main(int argc, char *argv[]) {
 	if(argc == 6) {
+		if(check_arg_validity(argv) != SUCCESS)
+			return(FAILURE);
 		struct timespec start, end;
+
 		clock_gettime(CLOCK_MONOTONIC, &start);
 		linear_motion_estimation(atoi(argv[1]), atoi(argv[2]), argv[3]);
+		clock_gettime(CLOCK_MONOTONIC, &end);
+		printf("Linear motion estimation total ");
+		calculate_difference(start, end, 1);
+
+		printf("\n");
+
+		clock_gettime(CLOCK_MONOTONIC, &start);
 		parallel_motion_estimation(atoi(argv[1]), atoi(argv[2]), argv[3], atoi(argv[4]));
 		clock_gettime(CLOCK_MONOTONIC, &end);
-		calculate_difference(start, end);
-		return 0;
+		printf("Parallel motion estimation total ");
+		calculate_difference(start, end, 1);
+
+		return(SUCCESS);
 	} else {
 		printf("Invalid number of arguements. Exiting.\n");
-		return -1;
+		return(FAILURE);
 	}
 }
